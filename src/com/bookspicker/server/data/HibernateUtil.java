40dc0bd3e105826f0a0ce53562bae45676ac0c4f
Rodrigo Ipince <rodrigo.ipince@gmail.com>
2012-08-27 18:06:26 -0700

    adds db instructions for running locally

--- src/com/bookspicker/server/data/HibernateUtil.java
+++ src/com/bookspicker/server/data/HibernateUtil.java
@@ -4,31 +4,31 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-	
+
     private static SessionFactory SESSION_FACTORY;
-    
+
     private enum Config {
-    	PROD, TEST, WIPE;
+        PROD, TEST, WIPE;
     }
-    
-    private static final Config CONFIG = Config.TEST;
-	public static boolean testDb = false;
-	public static boolean liveTest = false;
-	public static boolean wipeData = false;
+
+    private static final Config CONFIG = Config.WIPE;
+    public static boolean testDb = false;
+    public static boolean liveTest = false;
+    public static boolean wipeData = false;
 
     private static SessionFactory buildSessionFactory() {
         try {
-        	switch (CONFIG) {
-        	case PROD: // real data (unless running locally)
-        		return new Configuration().configure().buildSessionFactory();
-        	case TEST: // test data
-        		return new Configuration().configure("hibernate.test.cfg.xml").buildSessionFactory();
-        	case WIPE: // re-generate schema
-        		return new Configuration().configure("hibernate.wipedata.cfg.xml").buildSessionFactory();
-        	default: // test
-        		return new Configuration().configure("hibernate.test.cfg.xml").buildSessionFactory();
-        	}
-      /*      // Create the SessionFactory from hibernate config file
+            switch (CONFIG) {
+            case PROD: // real data (unless running locally)
+                return new Configuration().configure().buildSessionFactory();
+            case TEST: // test data
+                return new Configuration().configure("hibernate.test.cfg.xml").buildSessionFactory();
+            case WIPE: // re-generate schema
+                return new Configuration().configure("hibernate.wipedata.cfg.xml").buildSessionFactory();
+            default: // test
+                return new Configuration().configure("hibernate.test.cfg.xml").buildSessionFactory();
+            }
+            /*      // Create the SessionFactory from hibernate config file
         	if (testDb) {
         		if (liveTest) {
         			return new Configuration().configure("hibernate.test.live.cfg.xml").buildSessionFactory();
@@ -42,9 +42,9 @@ public class HibernateUtil {
         			}
         		}
         	} else {*/
-        		// regular hibernate.cfg.xml - used in production!
-//        		return new Configuration().configure().buildSessionFactory();
-        	//}
+            // regular hibernate.cfg.xml - used in production!
+            //        		return new Configuration().configure().buildSessionFactory();
+            //}
         } catch (Throwable ex) {
             // TODO: Make sure you log the exception, as it might be swallowed
             System.err.println("Initial SessionFactory creation failed." + ex);
@@ -53,8 +53,8 @@ public class HibernateUtil {
     }
 
     public static synchronized SessionFactory getSessionFactory() {
-    	if (SESSION_FACTORY == null)
-    		SESSION_FACTORY = buildSessionFactory();
+        if (SESSION_FACTORY == null)
+            SESSION_FACTORY = buildSessionFactory();
         return SESSION_FACTORY;
     }
 
