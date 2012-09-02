61a6bd5cae2f1b795cf5bc4efa42c15f5e3077ce
Rodrigo Ipince <rodrigo.ipince@gmail.com>
2012-09-02 07:06:54 -0700

    fix the way the course catalog link is displayed for classes

--- src/com/bookspicker/client/view/SearchPageUnified.java
+++ src/com/bookspicker/client/view/SearchPageUnified.java
@@ -52,7 +52,7 @@ import com.google.gwt.user.client.ui.Widget;
 public class SearchPageUnified extends Composite implements HasHeader {
 
     private static SearchPageUnifiedUiBinder uiBinder = GWT
-    .create(SearchPageUnifiedUiBinder.class);
+            .create(SearchPageUnifiedUiBinder.class);
 
     interface SearchPageUnifiedUiBinder extends UiBinder<Widget, SearchPageUnified> {
     }
@@ -81,7 +81,7 @@ public class SearchPageUnified extends Composite implements HasHeader {
     // Used to keep track of which results to render
     // on the page
     private static int offerRequestCounter = 0;
-    
+
     private static Map<Book, List<Offer>> bookOffers = new HashMap<Book, List<Offer>>();
 
     public SearchPageUnified() {
@@ -107,7 +107,7 @@ public class SearchPageUnified extends Composite implements HasHeader {
             GWT.log("No change in state - doing nothing", null);
             return;
         }
-        
+
         // If no bundle params, load what's in the cookie
         // Note: Cookies actually make this really complicated,
         // so I'm removing them for now
@@ -190,7 +190,7 @@ public class SearchPageUnified extends Composite implements HasHeader {
         public void onSelection(SelectionEvent<Suggestion> event) {
             findBooks(resultsView.autoCompleteBox.getText(), resultsView.autoCompleteBox.getDisplayedQuery());
         }
-    
+
         private void findBooks(String query, final String displayedQuery) {
             // Set history stuff
             currentQuery = query;
@@ -211,234 +211,227 @@ public class SearchPageUnified extends Composite implements HasHeader {
         }
     }
 
-    
+
     public static ClickHandler buyHandler = new ClickHandler() {
-    	@Override
-    	public void onClick(ClickEvent event) {
-    		BuyOfferButton button = (BuyOfferButton) event.getSource();
+        @Override
+        public void onClick(ClickEvent event) {
+            BuyOfferButton button = (BuyOfferButton) event.getSource();
 
-    		Book book = button.getBook();
-    		Offer clickedOffer = button.getOffer();
-    		List<Offer> competingOffers = bookOffers.get(book);
+            Book book = button.getBook();
+            Offer clickedOffer = button.getOffer();
+            List<Offer> competingOffers = bookOffers.get(book);
 
-    		// Save stat
-    		statService.logBuyClick(book.getIsbn(),
-    				clickedOffer, competingOffers,
-    				emptyCallback);
+            // Save stat
+            statService.logBuyClick(book.getIsbn(),
+                    clickedOffer, competingOffers,
+                    emptyCallback);
 
-    		// Open window with offer
-    		Window.open(button.getOffer().getUrl(), "_blank", "");
-    	}
+            // Open window with offer
+            Window.open(button.getOffer().getUrl(), "_blank", "");
+        }
     };
 
 
     // ============= Callbacks =============
 
-    	/**
-    	 * Returns an AsyncCallback to be used in QueryService.getBookInfo().
-    	 * 
-    	 * Depending on insertInBundle, the callback will either insert the corresponding
-    	 * books (returned by the QueryService) into the bundle or into the
-    	 * search result area.
-    	 */
+    /**
+     * Returns an AsyncCallback to be used in QueryService.getBookInfo().
+     * 
+     * Depending on insertInBundle, the callback will either insert the corresponding
+     * books (returned by the QueryService) into the bundle or into the
+     * search result area.
+     */
 
     public static AsyncCallback<List<Offer>> getOfferCallback(final ResultsBookView bookView) {
-    	return new AsyncCallback<List<Offer>>() {
-    		@Override
-    		public void onSuccess(List<Offer> result) {
-				bookView.showOffers(result);
-				SearchPageUnified.bookOffers.put(bookView.getBook(), result);
-    		}
+        return new AsyncCallback<List<Offer>>() {
+            @Override
+            public void onSuccess(List<Offer> result) {
+                bookView.showOffers(result);
+                SearchPageUnified.bookOffers.put(bookView.getBook(), result);
+            }
 
-    		@Override
-    		public void onFailure(Throwable caught) {
-    			bookView.reset();
-    			GWT.log(caught.getMessage(), caught);
-    		}
-    	};
+            @Override
+            public void onFailure(Throwable caught) {
+                bookView.reset();
+                GWT.log(caught.getMessage(), caught);
+            }
+        };
     }
 
     private static AsyncCallback<Void> emptyCallback = new AsyncCallback<Void>() {
-    	@Override
-    	public void onSuccess(Void result) {
-    		GWT.log("SearchPage - onSuccess at empty callback", null);
-    	}
-    	@Override
-    	public void onFailure(Throwable caught) {
-    		GWT.log("SearchPage - onFailure at empty callback", null);
-    	}
+        @Override
+        public void onSuccess(Void result) {
+            GWT.log("SearchPage - onSuccess at empty callback", null);
+        }
+        @Override
+        public void onFailure(Throwable caught) {
+            GWT.log("SearchPage - onFailure at empty callback", null);
+        }
     };
 
     // ============= Convenience methods =============
 
 
     private static void displayOffers(ResultsBookView bookView) {
-    	bookView.showLoading();
-    	offerRequestCounter++;
-    	GWT.log("SearchPage - Sending request #" + offerRequestCounter + " for offers", null);
-    	//TODO: Figure out why things are crashing with null pointer exceptions for non-MIT schools.
-    	queryService.getOffersForBook(BooksPicker.getSchool(), bookView.getBook(), true, getOfferCallback(bookView));
+        bookView.showLoading();
+        offerRequestCounter++;
+        GWT.log("SearchPage - Sending request #" + offerRequestCounter + " for offers", null);
+        //TODO: Figure out why things are crashing with null pointer exceptions for non-MIT schools.
+        queryService.getOffersForBook(BooksPicker.getSchool(), bookView.getBook(), true, getOfferCallback(bookView));
     }
 
     private static void clearSearch() {
-    	currentQuery = "";
-    	resultsView.searchTextBox.reset();
-    	resultsView.autoCompleteBox.cleanSavedQuery();
-    	resultsView.clearResults();
+        currentQuery = "";
+        resultsView.searchTextBox.reset();
+        resultsView.autoCompleteBox.cleanSavedQuery();
+        resultsView.clearResults();
     }
 
     public static void reset(){
-    	clearSearch();
+        clearSearch();
     }
 
     private static void populateSearchResults(Map<String, List<Item>> results, String displayedQuery) {
 
-    	List<Item> result;
-    	// used to determine whether to show a header for each query or not
-    	boolean multipleQueries = results.keySet().size() > 1;
-    	for (String query : results.keySet()) {
-    		result = results.get(query);
+        List<Item> result;
+        // used to determine whether to show a header for each query or not
+        boolean multipleQueries = results.keySet().size() > 1;
+        for (String query : results.keySet()) {
+            result = results.get(query);
 
-    		if (result == null) {
-    			// class not found
-    			resultsView.addInfoMessage(CLASS_NOT_FOUND_MSG + " matching \"" + displayedQuery + "\"");
-    		} else {
-    			if (result.isEmpty()) {
-    				// No books found -> display differently depending on query type
-    				if (query.matches(Constants.CLASS_REGEX_STR)) {
-    					resultsView.addInfoMessage(NO_BOOKS_FOUND_MSG + " for " + displayedQuery + "." +
-    							"<br /><br />This class probably requires no books, but we cannot guarantee it. Here's the <a href='http://course.mit.edu/" + displayedQuery + "' target='_blank'>class website</a> in case you wish to double-check." +
-    							"<br /><br />Let us know if we're missing any of " + displayedQuery + "'s books through the feedback tab on the right.");
-    				} else {
-    					resultsView.addInfoMessage("<br />"+NO_BOOKS_FOUND_MSG + " matching \"" + displayedQuery + "\"");
-    				}
-    			} else {
+            if (result == null) {
+                // class not found
+                resultsView.addInfoMessage(CLASS_NOT_FOUND_MSG + " matching \"" + displayedQuery + "\"");
+            } else {
+                if (result.isEmpty()) {
+                    // No books found -> display differently depending on query type
+                    if (query.matches(Constants.CLASS_REGEX_STR)) {
+                        resultsView.addInfoMessage(NO_BOOKS_FOUND_MSG + " for " + displayedQuery + "." +
+                                "<br /><br />This class probably requires no books, but we cannot guarantee it. Here's the <a href='http://course.mit.edu/" + displayedQuery + "' target='_blank'>class website</a> in case you wish to double-check." +
+                                "<br /><br />Let us know if we're missing any of " + displayedQuery + "'s books through the feedback tab on the right.");
+                    } else {
+                        resultsView.addInfoMessage("<br />"+NO_BOOKS_FOUND_MSG + " matching \"" + displayedQuery + "\"");
+                    }
+                } else {
 
-    				// Show ClassBooks first
-    				ClassBook cb;
-    				boolean headerShown = false;
-    				for (Item item : result) {
-    					//TODO (Sinchan) - can you please change the info message to an error message. See how it looks when 
-    					// you query for multiple books and one of the ISBN is broken. (e.g: 9780262033844, 6.003)
+                    // Show ClassBooks first
+                    ClassBook cb;
+                    boolean headerShown = false;
+                    for (Item item : result) {
+                        //TODO (Sinchan) - can you please change the info message to an error message. See how it looks when
+                        // you query for multiple books and one of the ISBN is broken. (e.g: 9780262033844, 6.003)
 
-    					if (item == null) {
-    						resultsView.addErrorMessage(NO_BOOKS_FOUND_MSG + " matching \"" + query + "\"<br />");
-    						headerShown = true;
-    					}
+                        if (item == null) {
+                            resultsView.addErrorMessage(NO_BOOKS_FOUND_MSG + " matching \"" + query + "\"<br />");
+                            headerShown = true;
+                        }
 
-    					if (item instanceof ClassBook) {
-    						cb = (ClassBook) item;
-    						// Only display the class title for the first book!
-    						if (!headerShown) {
-    							if (cb.getSchoolClass().getTitle()!= null){
-    								String classCode = cb.getSchoolClass().getCode();
-    								String fullTitle = classCode + cb.getSchoolClass().getTitle() + " ";
-    								String linkToCatalog = "<a style='font-weight: normal' href='http://student.mit.edu/catalog/search.cgi?search='"+classCode+"&style=verbatim' target='_blank'>// MIT Subject Listing //</a>";
-    								if (cb.getSchoolClass().getSchool().equals(School.MIT)){
-    									resultsView.addInfoMessage("Books for " + fullTitle + linkToCatalog);
-    								} else {
-    									resultsView.addInfoMessage("Books for " + fullTitle);
-    								}
-    							}
-    							else {
-    								String classCode = cb.getSchoolClass().getCode() + " ";
-    								String linkToCatalog = "<a style='font-weight: normal' href='http://student.mit.edu/catalog/search.cgi?search='"+classCode+"&style=verbatim' target='_blank'>// MIT Subject Listing //</a>";
-    								if (cb.getSchoolClass().getSchool().equals(School.MIT)){
-    									resultsView.addInfoMessage("Books for " + classCode + linkToCatalog);
-    								} else {
-    									resultsView.addInfoMessage("Books for " + classCode);
-    								}
-    							}
-
-    							headerShown = true;
-    						}
-    						resultsView.addClassBook(cb);
-    					} else if (item instanceof Book) {
-    						// Only show header for regular title search if many queries were made (to distinguish)
-    						if (multipleQueries && !headerShown) {
-    							resultsView.addInfoMessage("Books matching \"" + query + "\":");
-    							headerShown = true;
-    						}
-    						resultsView.addBook((Book) item);
-    					}
-    				}
-    			}
-    		}
-    	}
+                        if (item instanceof ClassBook) {
+                            cb = (ClassBook) item;
+                            // Only display the class title for the first book!
+                            if (!headerShown) {
+                                String fullTitle = cb.getSchoolClass().getCode();
+                                if (cb.getSchoolClass().getTitle() != null) {
+                                    fullTitle += " - " + cb.getSchoolClass().getTitle();
+                                }
+                                if (cb.getSchoolClass().getSchool().equals(School.MIT)
+                                        && cb.getSchoolClass().getClassInfoUrl() != null) {
+                                    String linkToCatalog = "<a style='font-weight: normal' href='"
+                                            + cb.getSchoolClass().getClassInfoUrl()
+                                            + "' target='_blank'>" + fullTitle + "</a>";
+                                    resultsView.addInfoMessage("Books for " + linkToCatalog);
+                                } else {
+                                    resultsView.addInfoMessage("Books for " + fullTitle);
+                                }
+                                headerShown = true;
+                            }
+                            resultsView.addClassBook(cb);
+                        } else if (item instanceof Book) {
+                            // Only show header for regular title search if many queries were made (to distinguish)
+                            if (multipleQueries && !headerShown) {
+                                resultsView.addInfoMessage("Books matching \"" + query + "\":");
+                                headerShown = true;
+                            }
+                            resultsView.addBook((Book) item);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     // ============= History stuff =============
 
     private static void setHistoryToken(boolean addToStack) {
-    	currentToken = buildToken();
-    	Cookies.setCookie("searchState", currentToken);
-    	if (addToStack)
-    		History.newItem(currentToken, false);
+        currentToken = buildToken();
+        Cookies.setCookie("searchState", currentToken);
+        if (addToStack)
+            History.newItem(currentToken, false);
 
     }
 
     private static String buildToken() {
-    	return buildToken(currentQuery, resultsView.autoCompleteBox.getDisplayedQuery());
+        return buildToken(currentQuery, resultsView.autoCompleteBox.getDisplayedQuery());
     }
 
     public static String buildToken(String queryParam, String displayedQuery) {
-    	StringBuilder sb = new StringBuilder();
-    	sb.append(HistoryToken.SEARCH_UNIFIED);
-    	if ((queryParam != null && !queryParam.isEmpty())) {
-    		sb.append(HistoryToken.PARAM_STARTER);
-    	}
+        StringBuilder sb = new StringBuilder();
+        sb.append(HistoryToken.SEARCH_UNIFIED);
+        if ((queryParam != null && !queryParam.isEmpty())) {
+            sb.append(HistoryToken.PARAM_STARTER);
+        }
 
-    	String delimeter = "";
+        String delimeter = "";
 
-    	if (queryParam != null && !queryParam.isEmpty()) {
-    		sb.append(delimeter);
-    		delimeter = HistoryToken.PARAM_DELIMETER;
-    		sb.append(HistoryToken.PARAM_QUERY);
-    		sb.append("=");
-    		sb.append(queryParam);
-    		sb.append(HistoryToken.PARAM_DELIMETER);
-    		sb.append(HistoryToken.DISPLAYED_QUERY);
-    		sb.append("=");
-    		sb.append(displayedQuery);
-    	}
+        if (queryParam != null && !queryParam.isEmpty()) {
+            sb.append(delimeter);
+            delimeter = HistoryToken.PARAM_DELIMETER;
+            sb.append(HistoryToken.PARAM_QUERY);
+            sb.append("=");
+            sb.append(queryParam);
+            sb.append(HistoryToken.PARAM_DELIMETER);
+            sb.append(HistoryToken.DISPLAYED_QUERY);
+            sb.append("=");
+            sb.append(displayedQuery);
+        }
 
-    	return sb.toString();
+        return sb.toString();
     }
 
     @Override
     public void updateLoginContainer() {
-    	headerView.updateLoginContainer();
+        headerView.updateLoginContainer();
     }
 
     @Override
     protected void onLoad() {
-    	super.onLoad();
-    	headerViewWrapper.setWidget(headerView);
-    	headerView.setSelectedMenuItem(Page.SEARCH);
+        super.onLoad();
+        headerViewWrapper.setWidget(headerView);
+        headerView.setSelectedMenuItem(Page.SEARCH);
     }
 
     public static class SearchCallback implements AsyncCallback<Map<String, List<Item>>> {
-    	private final String  _displayedQuery; 
+        private final String  _displayedQuery;
 
-    	public SearchCallback(String displayedQuery) {
-    		_displayedQuery = displayedQuery;
-    	}
+        public SearchCallback(String displayedQuery) {
+            _displayedQuery = displayedQuery;
+        }
 
-    	@Override
-    	public void onFailure(Throwable caught) {
-    		resultsView.clearResults();
-    		resultsView.loadingIcon.addStyleName(Resources.INSTANCE.style().loadingIconHidden());
-    		// Do nothing....
-    		GWT.log("There was an error retrieving the books", null);
+        @Override
+        public void onFailure(Throwable caught) {
+            resultsView.clearResults();
+            resultsView.loadingIcon.addStyleName(Resources.INSTANCE.style().loadingIconHidden());
+            // Do nothing....
+            GWT.log("There was an error retrieving the books", null);
 
-    	}
+        }
 
-    	@Override
-    	public void onSuccess(Map<String, List<Item>> result) {
-    		resultsView.clearResults();
-    		populateSearchResults(result, _displayedQuery);
-    		resultsView.loadingIcon.addStyleName(Resources.INSTANCE.style().loadingIconHidden());
-    	}
+        @Override
+        public void onSuccess(Map<String, List<Item>> result) {
+            resultsView.clearResults();
+            populateSearchResults(result, _displayedQuery);
+            resultsView.loadingIcon.addStyleName(Resources.INSTANCE.style().loadingIconHidden());
+        }
     }
 
 }
